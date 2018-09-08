4f3161b9b8c058f8a91c70021da3019837deddd9
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-09-08 20:15:32 +0100

    Blackjack

---
+++ blackjack.rb
@@ -0,0 +1,61 @@
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
+def deal_card
+  rand(1..11)
+end
+
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  card_total
+
+end
+
+def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
+end
+
+def get_user_input
+  gets.chomp
+end
+
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+end
+
+def initial_round
+  firstcard = deal_card
+  secoundcard = deal_card
+  total = firstcard + secoundcard
+  display_card_total(total)
+end
+
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+  if input == "s"
+    return card_total
+  elsif input == "h"
+    next_card = deal_card
+    card_total = card_total + next_card
+  else
+    invalid_command
+  end
+
+end
+
+def invalid_command
+  puts "Please enter a valid command"
+  prompt_user
+end
+
+def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
+  end
+  end_game(card_total)
+end
