737fcd804195e881079413a31fcebacb5b6ba83b
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-09-08 17:11:27 +0100

    This is all about loops

---
+++ loops.rb
@@ -0,0 +1,53 @@
+def loop_iterator(number_of_times)
+  counter = 0
+  phrase = "This is the loop iterator!"
+  loop do
+      if counter == number_of_times
+        break
+      else
+        puts phrase
+        counter += 1
+      end
+    end
+end
+
+def times_iterator(number_of_times)
+
+  phrase = "This is the Times iterator!"
+  number_of_times.times do
+    puts phrase
+  end
+end
+
+def while_iterator(number_of_times)
+  phrase = "This is the while itertor!!"
+  counter = 0
+  while number_of_times != counter
+    puts phrase
+    counter += 1
+  end
+end
+
+def until_iterator(number_of_times)
+  phrase = "This is the until iterator!"
+  counter = 0
+  until counter ==  number_of_times
+    puts phrase
+    counter += 1
+  end
+
+end
+
+def for_iterator(number_of_times)
+  range = 1..number_of_times
+  phrase = "This is the for iterator!"
+  for items in range
+    puts phrase
+  end
+end
+
+loop_iterator(1)
+times_iterator(2)
+while_iterator(3)
+until_iterator(4)
+for_iterator(5)
